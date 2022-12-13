abe1f3460ea26579d30542d55c661ba40d18399e
yacine.maghezzi <yacine.maghezzi@sonarsource.com>
2022-12-13 18:21:50 +0100

    Updated python regex functions

--- comp-cli/src/person.py
+++ comp-cli/src/person.py
@@ -62,3 +62,13 @@ def hotspot2(ip2):
 
 def configure_app(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:@domain.com"
+    re.compile(r"([")
+    re.sub(r"([", input, "{")
+    re.compile(r"(\w+-(\d+)")
+
+def tryregex(dateString):
+    p = re.compile(r"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
+
+    if p.match(dateString):
+        handleDate(dateString)
+
