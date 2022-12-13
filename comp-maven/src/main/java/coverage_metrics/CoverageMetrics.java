cd9f6fd7ee0b33e8ced7ace98475901e358ebe55
yacine.maghezzi <yacine.maghezzi@sonarsource.com>
2022-12-13 18:24:45 +0100

    remove myscret

--- comp-maven/src/main/java/coverage_metrics/CoverageMetrics.java
+++ comp-maven/src/main/java/coverage_metrics/CoverageMetrics.java
@@ -99,10 +99,8 @@ private int generateRandom() throws java.io.UnsupportedEncodingException{
   return v;
 }
 
-  private static final String MY_SECRET = "47828a8dd77ee1eb9dde2d5e93cb221ce8c32b37";
 
   public static void main(String[] args) {
-    MyClass.callMyService(MY_SECRET);
     String message = "myscret";
     System.out.println(message.toString());
 
