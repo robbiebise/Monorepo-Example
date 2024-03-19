7616ea9a2130c0a875c9b53dd1a5c90539a69e2e
Robbie Bise <theil.bise@sonarsource.com>
2024-03-19 13:35:04 -0500

    updates

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run each build.sh script in underlying comp-* directories
-for d in *; do
-    if [[ -d $d ]] && [[ $d =~ comp-.* ]]
-    then
-        cd $d; ./build.sh $*; cd -
-    fi
-done
